97c2221d3c904dda2da99130b793eab74933290b
Lucas Cavalcante de Oliveira <lukas.ass0206@gmail.com>
2025-12-20 14:38:58 -0300

    Arquivos separados e organizados

--- week1-c/Mario-Less/mario.c
+++ week1-c/Mario-Less/mario.c
@@ -3,32 +3,32 @@
 
 int main(void)
 {
-    // Declare the variable that will store the pyramid height
+    //Declara a váriavel que vai armazenar a altura da pirâmide
     int altura;
 
-    // Loop until the user enters a valid number
+    //Loop até que o usuário digite um número válido (entre 1 à 8)
     do
     {
         altura = get_int("Height: ");
     }
     while (altura < 1 || altura > 8);
 
-    // Loop that controls each row of the pyramid
+    //Loop para controlar as linhas da pirâmide
     for (int linha = 0; linha < altura; linha++)
     {
-        // Loop to print the left spaces, decreasing on each row
+        //loop para imprimir os espaços à esquerda, diminui a cada linha
         for (int espacos = 0; espacos < altura - linha - 1; espacos++)
         {
             printf(" ");
         }
 
-        // Loop to print the blocks (#), increasing on each row
+        //Loop para imprimir os blocos(#), aumenta a quantidade a cada linha
         for (int coluna = 0; coluna <= linha; coluna++)
         {
             printf("#");
         }
 
-        // Print a new line after finishing the current row
+        // Quebra de linha após imprimir todos os espaços e blocos da linha atual
         printf("\n");
     }
 }
